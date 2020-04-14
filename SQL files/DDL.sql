f084a95241b9e5f259c9a8aa401962bcd4b747fb
Henrique Fonseca <hdosprazeres@mun.ca>
2020-04-14 09:52:18 -0400

    Added queries file

--- SQL files/DDL.sql
+++ SQL files/DDL.sql
@@ -9,11 +9,22 @@ CREATE TABLE public.books
     price numeric(10,2),
     genre character varying(20) COLLATE pg_catalog."default",
     CONSTRAINT books_pkey PRIMARY KEY (isbn)
-)
+);
+
+CREATE TABLE public.users
+(
+    first_name character varying(20) COLLATE pg_catalog."default",
+    last_name character varying(20) COLLATE pg_catalog."default",
+    email character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    billing_address character varying(20) COLLATE pg_catalog."default",
+    password character varying(8) COLLATE pg_catalog."default",
+    CONSTRAINT users_pkey PRIMARY KEY (email)
+);
+
 
 CREATE TABLE public.cart
 (
-    item_nb integer NOT NULL DEFAULT nextval('cart_item_nb_seq'::regclass),
+    item_nb serial,
     email character varying(20) COLLATE pg_catalog."default" NOT NULL,
     qty numeric(2,0) NOT NULL DEFAULT 1,
     isbn numeric(13,0) NOT NULL,
@@ -27,7 +38,7 @@ CREATE TABLE public.cart
         ON UPDATE NO ACTION
         ON DELETE CASCADE,
     CONSTRAINT cart_qty_check CHECK (qty > 0::numeric)
-)
+);
 
 CREATE TABLE public.inventory
 (
@@ -35,48 +46,17 @@ CREATE TABLE public.inventory
     qty_in_stock numeric(3,0),
     CONSTRAINT inventory_pkey PRIMARY KEY (isbn),
     CONSTRAINT inventory_qty_in_stock_check CHECK (qty_in_stock > 10::numeric)
-)
-
-CREATE TABLE public.order_details
-(
-    order_id integer NOT NULL,
-    isbn numeric(13,0),
-    item_nb integer NOT NULL,
-    qty integer NOT NULL,
-    amount_paid numeric(6,2),
-    CONSTRAINT order_details_pkey PRIMARY KEY (order_id, item_nb),
-    CONSTRAINT order_details_isbn_fkey FOREIGN KEY (isbn)
-        REFERENCES public.inventory (isbn) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
-    CONSTRAINT order_details_order_id_fkey FOREIGN KEY (order_id)
-        REFERENCES public.placed_by (order_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
-    CONSTRAINT order_details_qty_check CHECK (qty > 0)
-)
+);
 
 
 CREATE TABLE public.orders
 (
-    order_id integer NOT NULL DEFAULT nextval('orders_order_id_seq'::regclass),
+    order_id serial,
     date_of_purchase timestamp without time zone NOT NULL DEFAULT CURRENT_DATE,
     shipping_address character varying(20) COLLATE pg_catalog."default" NOT NULL,
     method_of_payment character varying(15) COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT orders_pkey PRIMARY KEY (order_id)
-)
-
-CREATE TABLE public.phone_numbers
-(
-    email character varying(20) COLLATE pg_catalog."default" NOT NULL,
-    phonenumber numeric(7,0) NOT NULL,
-    CONSTRAINT phone_numbers_pkey PRIMARY KEY (email, phonenumber),
-    CONSTRAINT email FOREIGN KEY (email)
-        REFERENCES public.users (email) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE CASCADE
-        NOT VALID
-)
+);
 
 CREATE TABLE public.placed_by
 (
@@ -91,7 +71,48 @@ CREATE TABLE public.placed_by
         REFERENCES public.orders (order_id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
-)
+);
+
+CREATE TABLE public.order_details
+(
+    order_id integer NOT NULL,
+    isbn numeric(13,0),
+    item_nb integer NOT NULL,
+    qty integer NOT NULL,
+    amount_paid numeric(6,2),
+    CONSTRAINT order_details_pkey PRIMARY KEY (order_id, item_nb),
+    CONSTRAINT order_details_isbn_fkey FOREIGN KEY (isbn)
+        REFERENCES public.books (isbn) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT order_details_order_id_fkey FOREIGN KEY (order_id)
+        REFERENCES public.placed_by (order_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT order_details_qty_check CHECK (qty > 0)
+);
+
+CREATE TABLE public.phone_numbers
+(
+    email character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    phonenumber numeric(7,0) NOT NULL,
+    CONSTRAINT phone_numbers_pkey PRIMARY KEY (email, phonenumber),
+    CONSTRAINT email FOREIGN KEY (email)
+        REFERENCES public.users (email) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE
+        NOT VALID
+);
+
+CREATE TABLE public.publishers
+(
+    email character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    publisher_name character varying(20) COLLATE pg_catalog."default",
+    address character varying(25) COLLATE pg_catalog."default",
+    bank_account numeric NOT NULL,
+    CONSTRAINT publishers_pkey PRIMARY KEY (email),
+    CONSTRAINT bankcheck CHECK (length(bank_account::character(255)) = 10)
+);
 
 CREATE TABLE public.published_by
 (
@@ -108,24 +129,7 @@ CREATE TABLE public.published_by
         ON UPDATE NO ACTION
         ON DELETE CASCADE,
     CONSTRAINT published_by_cut_check CHECK (cut >= 5 AND cut <= 25)
-)
+);
 
-CREATE TABLE public.publishers
-(
-    email character varying(20) COLLATE pg_catalog."default" NOT NULL,
-    publisher_name character varying(20) COLLATE pg_catalog."default",
-    address character varying(25) COLLATE pg_catalog."default",
-    bank_account numeric NOT NULL,
-    CONSTRAINT publishers_pkey PRIMARY KEY (email),
-    CONSTRAINT bankcheck CHECK (length(bank_account::character(255)) = 10)
-)
 
-CREATE TABLE public.users
-(
-    first_name character varying(20) COLLATE pg_catalog."default",
-    last_name character varying(20) COLLATE pg_catalog."default",
-    email character varying(20) COLLATE pg_catalog."default" NOT NULL,
-    billing_address character varying(20) COLLATE pg_catalog."default",
-    password character varying(8) COLLATE pg_catalog."default",
-    CONSTRAINT users_pkey PRIMARY KEY (email)
-)
+
