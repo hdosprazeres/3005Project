99a1b672469640838e24ee1a6b35c308156c9e0a
Henrique Fonseca <hdosprazeres@mun.ca>
2020-04-14 10:13:18 -0400

    Added queries file

--- SQL files/DDL.sql
+++ SQL files/DDL.sql
@@ -131,5 +131,29 @@ CREATE TABLE public.published_by
     CONSTRAINT published_by_cut_check CHECK (cut >= 5 AND cut <= 25)
 );
 
+-- Table: public.session
+
+-- DROP TABLE public.session;
+
+CREATE TABLE public.session
+(
+    sid character varying COLLATE pg_catalog."default" NOT NULL,
+    sess json NOT NULL,
+    expire timestamp(6) without time zone NOT NULL,
+    CONSTRAINT session_pkey PRIMARY KEY (sid)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.session
+    OWNER to postgres;
+-- Index: IDX_session_expire
+
+-- DROP INDEX public."IDX_session_expire";
+
+CREATE INDEX "IDX_session_expire"
+    ON public.session USING btree
+    (expire ASC NULLS LAST)
+    TABLESPACE pg_default;
 
 
